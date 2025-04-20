876b443372a75cb4176142b55931171f21c29a68
wadwdwd1 <imrbk123abc@proton.me>
2025-04-20 11:16:54 -0500

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing mineflayer-pathfinder..."
+npm install mineflayer-pathfinder
+
+echo "Installing mineflayer..."
+npm install mineflayer
+
+echo "Running index.js..."
+node index.js
